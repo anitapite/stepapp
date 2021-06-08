1ecc5bff8c59a32e4ce4c2a615b1e1d4bee75d6a
anitapite <anitapite@gmail.com>
2021-06-08 23:21:55 +0100

    think managed to add the new fields

--- steps/views.py
+++ steps/views.py
@@ -9,8 +9,20 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 class StepCount(forms.Form):
-    stepcount = forms.IntegerField(label="Stepcount")
-    activities = forms.CharField(label='Activities')
+    monday_stepcount = forms.IntegerField(label="monday_stepcount")
+    monday_activities = forms.CharField(label='monday_activities')
+    tuesday_stepcount = forms.IntegerField(label="tuesday_stepcount")
+    tuesday_activities = forms.CharField(label='tuesday_activities')
+    wednesday_stepcount = forms.IntegerField(label="wednesday_stepcount")
+    wednesday_activities = forms.CharField(label='wednesday_activities')
+    thursday_stepcount = forms.IntegerField(label="thursday_stepcount")
+    thursday_activities = forms.CharField(label='thursday_activities')
+    friday_stepcount = forms.IntegerField(label="friday_stepcount")
+    friday_activities = forms.CharField(label='friday_activities')
+    saturday_stepcount = forms.IntegerField(label="saturday_stepcount")
+    saturday_activities = forms.CharField(label='saturday_activities')
+    sunday_stepcount = forms.IntegerField(label="sunday_stepcount")
+    sunday_activities = forms.CharField(label='sunday_activities')
    
 
 class DateInput(forms.DateInput):
@@ -20,9 +32,23 @@ class DateInput(forms.DateInput):
 class StepForm(forms.ModelForm):
     class Meta:
         model = Step
-        fields = ['stepcount', 'activities', 'date']
+        fields = [
+            'monday_stepcount', 'monday_activities', 'monday_date', 
+            'tuesday_stepcount', 'tuesday_activities', 'tuesday_date',
+            'wednesday_stepcount', 'wednesday_activities', 'wednesday_date', 
+            'thursday_stepcount', 'thursday_activities', 'thursday_date',
+            'friday_stepcount', 'friday_activities', 'friday_date', 
+            'saturday_stepcount', 'saturday_activities', 'saturday_date',
+            'sunday_stepcount', 'sunday_activities', 'sunday_date',
+            ]
         widgets = {
-            'date': DateInput(),
+            'monday_date': DateInput(),
+            'tuesday_date': DateInput(),
+            'wednesday_date': DateInput(),
+            'thursday_date': DateInput(),
+            'friday_date': DateInput(),
+            'saturday_date': DateInput(),
+            'sunday_date': DateInput(),
         }
 
 
